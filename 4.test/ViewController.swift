7abc4aa53fa33ad4792931a0d479d9772b43ee69
ZalkarbekArapbaev <zalkarbek2022@gmail.com>
2022-11-23 20:37:30 +0600

    Initial Commit

---
+++ 4.test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4.test
+//
+//  Created by zalkarbek on 23/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
